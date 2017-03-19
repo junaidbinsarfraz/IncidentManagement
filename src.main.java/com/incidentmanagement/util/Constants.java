d36bc9d5da0f2ecb2b6821b9e21419be0a234da6
Juanid <junaidbinsarfraz@yahoo.com>
2017-03-19 22:36:00 +0500

    Adding additional fields

--- src.main.java/com/incidentmanagement/util/Constants.java
+++ src.main.java/com/incidentmanagement/util/Constants.java
@@ -2,13 +2,15 @@ package com.incidentmanagement.util;
 
 public interface Constants {
 	
-	public static String DECISION_ADMISSION_REJECT = "AK";
-	public static String STATUS_DE_INCIDENT = "AL";
-	public static String TYPE_DE_REFERENCE_EXTERNE = "AM";
-	public static String COMITE_DE_BALE = "AN";
-	public static String CODE_ETAT_ACTUAEL_DU_PERT_SUR = "AV";
-	public static String ETATE_DE_LA_PERTE = "AW";
-	public static String CLASSIFICATION_DE_LA_PERTE = "AX";
-	public static String NOTATION_DE_LA_PERTE_EN_REPUTATION = "AY";
+	public static String DECISION_ADMISSION_REJECT = "DECISION_ADMISSION_REJECT";
+	public static String STATUS_DE_INCIDENT = "STATUS_DE_INCIDENT";
+	public static String TYPE_DE_REFERENCE_EXTERNE = "TYPE_DE_REFERENCE_EXTERNE";
+	public static String COMITE_DE_BALE = "COMITE_DE_BALE";
+	public static String CODE_ETAT_ACTUAEL_DU_PERT_SUR = "CODE_ETAT_ACTUAEL_DU_PERT_SUR";
+	public static String ETATE_DE_LA_PERTE = "ETATE_DE_LA_PERTE";
+	public static String CLASSIFICATION_DE_LA_PERTE = "CLASSIFICATION_DE_LA_PERTE";
+	public static String NOTATION_DE_LA_PERTE_EN_REPUTATION = "NOTATION_DE_LA_PERTE_EN_REPUTATION";
+	public static String AGENCES = "AGENCES";
+	
 	
 }
